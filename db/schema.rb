25bd08a1a4fec8b796b770bb8d9ae45ebc3bfeb2
Nick Manton <mantonnick@outlook.com>
2026-02-20 13:58:31 +1100

    Created css system

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.1].define(version: 2026_02_20_014321) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "profiles", force: :cascade do |t|
     t.text "bio"
     t.datetime "created_at", null: false
