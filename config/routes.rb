d967638ca6a7516c9e43581c043e78db9352ac8b
Nick Manton <mantonnick@outlook.com>
2026-02-20 12:50:30 +1100

    Fixed devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  get "home/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "home#index"
+  devise_for :users
+  resources :profiles
+
+  # Optional 'interested in' functionality
+  # resources :interests, only: [:create, :index, :destroy]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
---
+++ db/migrate/20260220013826_create_profiles.rb
@@ -0,0 +1,15 @@
+class CreateProfiles < ActiveRecord::Migration[8.1]
+  def change
+    create_table :profiles do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :name
+      t.boolean :pro_badge
+      t.text :bio
+      t.string :instruments
+      t.string :genres
+      t.string :location
+
+      t.timestamps
+    end
+  end
+end
