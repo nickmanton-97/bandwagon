59c5378c6571620aeb386a0339a4dc7daf9e44d6
Nick Manton <mantonnick@outlook.com>
2026-02-24 18:44:38 +1100

    Pre-router fix

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,42 +1,45 @@
 class ProfilesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_profile, only: [:show, :edit, :update]
+  before_action :ensure_profile_exists, only: [:index]
 
-  # GET /profiles
   def index
     if params[:filter].present?
-      # Adjust depending on how instruments/genres are stored
-      @profiles = Profile.where("instruments @> ARRAY[?]::varchar[] OR genres @> ARRAY[?]::varchar[]", params[:filter], params[:filter])
+      @profiles = Profile.where(
+        "instruments @> ARRAY[?]::varchar[] OR genres @> ARRAY[?]::varchar[]",
+        params[:filter],
+        params[:filter]
+      )
     else
       @profiles = Profile.all
     end
   end
 
-  # GET /profiles/:id
   def show
   end
 
-  # GET /profiles/new
   def new
-    @profile = current_user.build_profile
+    if current_user.profile.present?
+      redirect_to profiles_path
+    else
+      @profile = current_user.build_profile
+    end
   end
 
-  # POST /profiles
   def create
     @profile = current_user.build_profile(profile_params)
+
     if @profile.save
-      redirect_to @profile, notice: "Profile successfully created."
+      redirect_to profiles_path, notice: "Profile successfully created."
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  # GET /profiles/:id/edit
   def edit
     redirect_to profiles_path, alert: "Not authorized" unless @profile.user == current_user
   end
 
-  # PUT/PATCH /profiles/:id
   def update
     if @profile.update(profile_params)
       redirect_to @profile, notice: "Profile successfully updated."
@@ -46,6 +49,12 @@ class ProfilesController < ApplicationController
   end
 
   private
+
+  def ensure_profile_exists
+    if user_signed_in? && current_user.profile.blank?
+      redirect_to new_profile_path
+    end
+  end
 
   def set_profile
     @profile = Profile.find(params[:id])
